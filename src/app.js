9a8079d1fba611065c77c446b0f2c472bfed797b
johannaalamo <134504529+johannaalamo@users.noreply.github.com>
2023-09-24 15:38:57 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["El perro", "Mi abuela", "Elon Musk", "Britney"];
+let action = ["se comió", "orinó en", "mató", "rompió"];
+let what = ["el internet", "mi ordenador", "mi proyecto"];
+let when = [
+  "antes de la clase",
+  "en el peor momento",
+  "mientras comía",
+  "mientras rezaba"
+];
+
+function generarExcusa() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+
+function actualizarExcusa() {
+  let excuse = generarExcusa();
+  document.getElementById("excuse").textContent = excuse;
+}
+
+document.getElementById("boton-generar").addEventListener("click", function() {
+  actualizarExcusa();
+});
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  actualizarExcusa();
 };
